c87e1015f9550d374efe6dea4ce33e9819ca0cf7
Lukas <lukas.willsie@mail.utoronto.ca>
2019-10-25 22:34:44 -0700

    Added toString methods to Rank and Suit

--- phase1/Game/app/src/main/java/com/example/game/level1/Rank.java
+++ phase1/Game/app/src/main/java/com/example/game/level1/Rank.java
@@ -23,4 +23,37 @@ public enum Rank {
     public int getValue(){
         return value;
     }
+
+    public String toString(){
+        switch(this){
+            case ACE:
+                return "A";
+            case TWO:
+                return "2";
+            case THREE:
+                return "3";
+            case FOUR:
+                return "4";
+            case FIVE:
+                return "5";
+            case SIX:
+                return "6";
+            case SEVEN:
+                return "7";
+            case EIGHT:
+                return "8";
+            case NINE:
+                return "9";
+            case TEN:
+                return "10";
+            case JACK:
+                return "11";
+            case QUEEN:
+                return "12";
+            case KING:
+                return "13";
+            default:
+                return "";
+        }
+    }
 }
--- phase1/Game/app/src/main/java/com/example/game/level1/Suit.java
+++ phase1/Game/app/src/main/java/com/example/game/level1/Suit.java
@@ -4,5 +4,20 @@ public enum Suit {
     SPADES,
     HEARTS,
     CLUBS,
-    DIAMONDS
+    DIAMONDS;
+
+    public String toString(){
+        switch(this){
+            case SPADES:
+                return "\u2660";
+            case HEARTS:
+                return "\u2661";
+            case CLUBS:
+                return "\u2663";
+            case DIAMONDS:
+                return "\u2662";
+            default:
+                return "";
+        }
+    }
 }
