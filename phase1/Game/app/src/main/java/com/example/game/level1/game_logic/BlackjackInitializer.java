0de0f5d1bc90f00ca4aa85555c81a9ffeb1a9fe4
Lukas <lukas.willsie@mail.utoronto.ca>
2019-10-26 14:21:58 -0700

    Added ButtonManager field to BlackjackLevelManager and updated BlackjackInitializer accordingly; added requirement to handle button clicks to LevelManager interface

--- phase1/Game/app/src/main/java/com/example/game/level1/game_logic/BlackjackInitializer.java
+++ phase1/Game/app/src/main/java/com/example/game/level1/game_logic/BlackjackInitializer.java
@@ -1,7 +1,9 @@
 package com.example.game.level1.game_logic;
 
+import android.view.View;
 import android.widget.TextView;
 
+import com.example.game.level1.display.ButtonManager;
 import com.example.game.level1.domain.Dealer;
 import com.example.game.level1.domain.Deck;
 import com.example.game.level1.domain.Player;
@@ -9,13 +11,13 @@ import com.example.game.level1.domain.Player;
 public class BlackjackInitializer implements LevelInitializer {
     private LevelManager gameManager;
 
-    public BlackjackInitializer(TextView userHand, TextView dealerHand) {
+    public BlackjackInitializer(TextView userHand, TextView dealerHand, ButtonManager buttonManager) {
         Player user = new Player();
         Dealer dealer = new Dealer();
         Deck deck = new Deck();
 
         InterfaceManager interfaceManager = new BlackjackInterfaceManager(user, dealer, userHand, dealerHand);
-        gameManager = new BlackjackLevelManager(user, dealer, deck, interfaceManager);
+        gameManager = new BlackjackLevelManager(user, dealer, deck, interfaceManager, buttonManager);
     }
 
     public LevelManager setup() {
--- phase1/Game/app/src/main/java/com/example/game/level1/game_logic/LevelManager.java
+++ phase1/Game/app/src/main/java/com/example/game/level1/game_logic/LevelManager.java
@@ -1,7 +1,11 @@
 package com.example.game.level1.game_logic;
 
+import android.view.View;
+
 public interface LevelManager {
     void setup();
 
     void play();
+
+    void userButtonClick(View view);
 }
