b8f286ecf381c78d7d56c6a6d125d1ce15918be9
Lukas <lukas.willsie@mail.utoronto.ca>
2019-10-25 22:12:44 -0700

    Added Dealer class, and added constructor to Player class

--- phase1/Game/app/src/main/java/com/example/game/level1/Player.java
+++ phase1/Game/app/src/main/java/com/example/game/level1/Player.java
@@ -2,6 +2,11 @@ package com.example.game.level1;
 
 public class Player {
     /**
+     * This player's hand
+     */
+    private Hand hand;
+
+    /**
      * Get this player's hand
      * @return this player's hand
      */
@@ -9,10 +14,13 @@ public class Player {
         return hand;
     }
 
-    /**
-     * This player's hand
-     */
-    private Hand hand;
+    public Player(){
+        hand = new Hand();
+    }
+
+    public Player(Hand hand){
+        this.hand = hand;
+    }
 
     /**
      * Add the given card to this player's hand
