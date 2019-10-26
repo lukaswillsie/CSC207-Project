ea88007bb782593bf09260c445fc9663e9498cdb
Valeria Fakhrutdinova <valeriia.fakhrutdinova@mail.utoronto.ca>
2019-10-25 20:30:31 -0400

    Create a new class GameManager

---
+++ phase1/Game/app/src/main/java/com/example/game/level2/GameManager.java
@@ -0,0 +1,26 @@
+package com.example.game.level2;
+import java.util.ArrayList;
+
+public class GameManager {
+    /**
+     * list of all games that have been played
+     */
+    ArrayList<Game> gameList;
+    /**
+     * Leader board keeps track of top three players.
+     */
+    int[] leaderBoard = new  int[3];
+    /**
+     * Rules for the game.
+     */
+    String rules;
+
+    /**
+     * Create a gameManager
+     */
+    public GameManager(){
+       this.gameList = new ArrayList<>();
+       this.rules = "";
+    }
+
+}
