4a158a82679ef805c522eb5c03fb4afbcd4efc50
Alexandra <alexandra.tran@outlook.com>
2019-10-25 20:09:23 -0400

    Added a new class called User.

---
+++ phase1/Game/app/src/main/java/com/example/game/level2/User.java
@@ -0,0 +1,28 @@
+package com.example.game.level2;
+
+public class User {
+    /**
+     * Username of this user
+     */
+    String username;
+
+    /**
+     * The array that stores all the games this user has played.
+     */
+    ArrayList<Game> games;
+
+    /**
+     * The best score this user currently holds out of all their games played.
+     */
+    int bestScore;
+
+    /**
+     * Create a new instance of a user.
+     * @param username  The username of this user.
+     */
+    User(String username) {
+        this.username = username;
+        games = new ArrayList<Game>();
+        bestScore = 0;
+    }
+}
