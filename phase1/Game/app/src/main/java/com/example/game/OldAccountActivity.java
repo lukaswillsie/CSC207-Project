9702c8a97101837abff269e5ae00e0c485fa41e5
Lukas <lukas.willsie@mail.utoronto.ca>
2019-10-27 16:32:18 -0700

    Created login activity for old account

---
+++ phase1/Game/app/src/main/java/com/example/game/OldAccountActivity.java
@@ -0,0 +1,14 @@
+package com.example.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OldAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_old_account);
+    }
+}
--- phase1/Game/app/src/main/java/com/example/game/StartActivity.java
+++ phase1/Game/app/src/main/java/com/example/game/StartActivity.java
@@ -2,7 +2,9 @@ package com.example.game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+    }
+
+    public void newAccount(View view){
+        Intent intent = new Intent(this, NewAccountActivity.class);
+        startActivity(intent);
+    }
+
+    public void oldAccount(View view){
+        Intent intent = new Intent(this, OldAccountActivity.class);
+        startActivity(intent);
     }
 }
