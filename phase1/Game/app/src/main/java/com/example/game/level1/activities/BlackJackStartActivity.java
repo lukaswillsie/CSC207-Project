d56faafa92e18d57ef179101378d90dee470cc2a
Lukas <lukas.willsie@mail.utoronto.ca>
2019-10-26 20:01:26 -0700

    Added a link to the pre-Blackjack level blurb

--- phase1/Game/app/src/main/java/com/example/game/level1/activities/BlackJackStartActivity.java
+++ phase1/Game/app/src/main/java/com/example/game/level1/activities/BlackJackStartActivity.java
@@ -2,7 +2,9 @@ package com.example.game.level1.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,6 +16,7 @@ public class BlackJackStartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.black_jack_start);
+        ((TextView)findViewById(R.id.blackjackBlurb)).setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     public void startGame(View view) {
