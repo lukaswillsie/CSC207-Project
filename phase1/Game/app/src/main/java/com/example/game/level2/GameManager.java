4cadc7c5ec0eb5dfa8b6d4b8392339e88dd3d9a3
Elexandra Tran <elexandra.tran@utoronto.ca>
2019-10-28 00:15:33 -0400

    Working on startNewGame method in GameManager class.

--- phase1/Game/app/src/main/java/com/example/game/level2/GameManager.java
+++ phase1/Game/app/src/main/java/com/example/game/level2/GameManager.java
@@ -3,9 +3,9 @@ import java.util.ArrayList;
 
 public class GameManager {
     /**
-     * list of all games that have been played
+     * The current game this game manager holds.
      */
-    ArrayList<Game> gameList;
+     Game game;
     /**
      * Leader board keeps track of top three players.
      */
@@ -19,13 +19,15 @@ public class GameManager {
      * Create a gameManager
      */
     public GameManager(){
-       this.gameList = new ArrayList<>();
+       this.game = new Game();
        this.rules = "";
     }
 
     // call this class whenever a new GTN game is about to be played
     public void startNewGame() {
-        Game newGame = new Game();
+        game = new Game();
+
+
     }
 
 }
