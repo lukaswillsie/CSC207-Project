cdcb01fe4370c2d221d54308097c29dafe909285
Lukas <lukas.willsie@mail.utoronto.ca>
2019-10-27 17:51:48 -0700

    Made it so that default activity is StartActivity instead of MainActivity. Created button Test that skips login process and goes right to MainActivity for game-testing purposes

--- phase1/Game/app/src/main/java/com/example/game/StartActivity.java
+++ phase1/Game/app/src/main/java/com/example/game/StartActivity.java
@@ -23,4 +23,9 @@ public class StartActivity extends AppCompatActivity {
         Intent intent = new Intent(this, OldAccountActivity.class);
         startActivity(intent);
     }
+
+    public void Test(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 }
