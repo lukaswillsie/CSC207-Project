78afecfc1750e7ebc1f2d769d480e61e99c58a35
Elexandra Tran <elexandra.tran@utoronto.ca>
2019-10-28 00:58:48 -0400

    Added a new activity called GameHighActivity.

--- phase1/Game/app/src/main/java/com/example/game/level2/GameHighActivity.java
+++ phase1/Game/app/src/main/java/com/example/game/level2/GameHighActivity.java
@@ -1,4 +1,17 @@
 package com.example.game.level2;
 
-public class GameHighActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.game.R;
+
+public class GameHighActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_high_activity);
+    }
 }
