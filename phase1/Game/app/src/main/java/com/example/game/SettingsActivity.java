9f6f0296f1fe3a94026e322a32d527a70802a7a2
Lukas <lukas.willsie@mail.utoronto.ca>
2019-10-27 21:51:16 -0700

    Made it so that a TextView shows the current selection above each SeekBar on the settings page and updates as the seekbar is moved

--- phase1/Game/app/src/main/java/com/example/game/SettingsActivity.java
+++ phase1/Game/app/src/main/java/com/example/game/SettingsActivity.java
@@ -3,6 +3,8 @@ package com.example.game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,5 +12,35 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        SeekBar numHandsBar = findViewById(R.id.numHandsSeekBar);
+        SeekBar numRoundsBar = findViewById(R.id.numRoundsSeekBar);
+        numHandsBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                String progressString = "" + progress;
+                ((TextView)findViewById(R.id.numHandsLabel)).setText(progressString);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+        numRoundsBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                String progressString = "" + progress;
+                ((TextView)findViewById(R.id.numRoundsLabel)).setText(progressString);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
     }
 }
