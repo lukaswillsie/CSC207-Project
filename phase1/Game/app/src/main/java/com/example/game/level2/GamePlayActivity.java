a56d35534ec12624c37905d1c87dddf96934d153
Elexandra Tran <elexandra.tran@utoronto.ca>
2019-10-27 23:49:28 -0400

    Added a method that retrieves the guess input from the user.

--- phase1/Game/app/src/main/java/com/example/game/level2/GamePlayActivity.java
+++ phase1/Game/app/src/main/java/com/example/game/level2/GamePlayActivity.java
@@ -4,14 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.game.R;
-class GamePlayActivity extends AppCompatActivity{
+class GamePlayActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_play_activity);
     }
-
 
 }
--- phase1/Game/app/src/main/java/com/example/game/level2/GameStartActivity1.java
+++ phase1/Game/app/src/main/java/com/example/game/level2/GameStartActivity1.java
@@ -2,6 +2,7 @@ package com.example.game.level2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.game.R;
 
@@ -11,4 +12,9 @@ public class GameStartActivity1 extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_start_activity1);
     }
+
+    public int getGuess() {
+        return Integer.valueOf(((TextView)findViewById(R.id.guessInput)).getText().toString());
+    }
+
 }
