a22914147847fb3c3663093f52f44b045d566d3d
Lukas <lukas.willsie@mail.utoronto.ca>
2019-10-29 22:28:49 -0700

    Added documentation to SettingsManagerBuilder

--- phase1/Game/app/src/main/java/com/example/game/services/SettingsManagerBuilder.java
+++ phase1/Game/app/src/main/java/com/example/game/services/SettingsManagerBuilder.java
@@ -3,6 +3,13 @@ package com.example.game.services;
 import android.content.Context;
 
 public class SettingsManagerBuilder {
+    /**
+     * Build a SettingsManager for the user with the given username, from the given Context
+     * @param context - the context that wants the SettingsManager
+     * @param username - the username of the user whose settings the SettingsManager is going to
+     *                 be working with
+     * @return a SettingsManager for the given user
+     */
     public SettingsManager build(Context context, String username){
         return new UserSettingsManager(context, username);
     }
