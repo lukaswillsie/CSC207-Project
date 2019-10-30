088abe4125597a1eab21d1f8afe8ef5e66b03134
Lukas <lukas.willsie@mail.utoronto.ca>
2019-10-29 22:39:51 -0700

    Added documentation to BlackjackPlayerManager

--- phase1/Game/app/src/main/java/com/example/game/level1/domain/BlackjackPlayerManager.java
+++ phase1/Game/app/src/main/java/com/example/game/level1/domain/BlackjackPlayerManager.java
@@ -3,22 +3,41 @@ package com.example.game.level1.domain;
 import static com.example.game.level1.domain.Rank.ACE;
 
 public class BlackjackPlayerManager {
+    /**
+     * The player that this BlackjackPlayerManager object will be managing
+     */
     private Player player;
 
+    /**
+     * Create a new BlackjackPlayerManager for a given player
+     * @param player - the player to be managed
+     */
     public BlackjackPlayerManager(Player player) {
         this.player = player;
     }
 
+    /**
+     * Add a card to the player's hand
+     * @param card - the card to add to the player's hand
+     */
     public void deal(Card card) {
         player.getHand().addCard(card);
     }
 
+    /**
+     * Adds the given array of Cards to the player's hand
+     * @param cards - the array of all the cards to be added to the player's hand
+     */
     public void deal(Card[] cards) {
         for (Card card : cards) {
             deal(card);
         }
     }
 
+    /**
+     * Compute the value of the player's hand according to the rules of Blackjack
+     * @return - the value of this player's hand, according to the rules of Blackjack
+     */
     public int computeBlackJackValue() {
         Hand hand = player.getHand();
         int numAces = 0;
