53e7dff7eba0e03caa78d908ee70eba71edb5e16
Lukas <lukas.willsie@mail.utoronto.ca>
2019-10-29 22:31:28 -0700

    Added documentation to ButtonManager

--- phase1/Game/app/src/main/java/com/example/game/level1/display/ButtonManager.java
+++ phase1/Game/app/src/main/java/com/example/game/level1/display/ButtonManager.java
@@ -11,6 +11,13 @@ import java.util.List;
 public class ButtonManager {
     private static List<Button> buttons;
 
+    /**
+     * Setup the ButtonManager for use. Must be called each time a new activity
+     * wants to use ButtonManager
+     *
+     * Gets all the buttons from the given activity and keeps them in a List
+     * @param activity - the activity that wants the ButtonManager
+     */
     public static void setup(Activity activity) {
         buttons = new ArrayList<>();
 
@@ -36,6 +43,13 @@ public class ButtonManager {
         }
     }
 
+    /**
+     * Disable the button with the given id - that is, make it unclickable
+     * The button's visibility will remain unchanged
+     *
+     * Does nothing if no button has the given id
+     * @param id - the id of the button to disable
+     */
     public static void disableButton(int id) {
         for (Button button : buttons) {
             if (button.getId() == id) {
@@ -45,6 +59,11 @@ public class ButtonManager {
         }
     }
 
+    /**
+     * Enable the button with the given id - that is, make it clickable
+     * The button's visibility will remain unchanged
+     * @param id - the id of the button to enable
+     */
     public static void enableButton(int id) {
         for (Button button : buttons) {
             if (button.getId() == id) {
@@ -54,6 +73,11 @@ public class ButtonManager {
         }
     }
 
+    /**
+     * Make the button with the given id invisible, or does nothing if it is already invisible
+     *
+     * @param id - the id of the button to make invisible
+     */
     public static void makeButtonInvisible(int id) {
         for (Button button : buttons) {
             if (button.getId() == id) {
@@ -63,6 +87,11 @@ public class ButtonManager {
         }
     }
 
+    /**
+     * Make the button with the given id visible, or does nothing if it is already visible
+     *
+     * @param id - the id of the button to make visible
+     */
     public static void makeVisible(int id) {
         for (Button button : buttons) {
             if (button.getId() == id) {
