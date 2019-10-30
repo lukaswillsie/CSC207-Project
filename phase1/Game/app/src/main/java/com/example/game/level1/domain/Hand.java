09605d177847d2fdc6c18796fd351697a7be8bd9
Lukas <lukas.willsie@mail.utoronto.ca>
2019-10-29 22:45:14 -0700

    Added documentation to Hand class

--- phase1/Game/app/src/main/java/com/example/game/level1/domain/Hand.java
+++ phase1/Game/app/src/main/java/com/example/game/level1/domain/Hand.java
@@ -1,5 +1,7 @@
 package com.example.game.level1.domain;
 
+import androidx.annotation.NonNull;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -25,21 +27,40 @@ public class Hand implements Iterable<Card> {
         this.hand.add(card);
     }
 
+    /**
+     * Return a card iterator for this object
+     * @return a card iterator for this object
+     */
+    @NonNull
     public Iterator<Card> iterator() {
         return new HandIterator();
     }
 
     private class HandIterator implements Iterator<Card> {
+        /**
+         * The index of the iterator with respect to hand
+         */
         private int index;
 
+        /**
+         * Create a new HandIterator
+         */
         HandIterator() {
             index = 0;
         }
 
+        /**
+         * Check if this iterator has another element to return
+         * @return - true if there is another element and false otherwise
+         */
         public boolean hasNext() {
             return index < hand.size();
         }
 
+        /**
+         * Get the next element from the iterator
+         * @return - the next element
+         */
         public Card next() {
             return hand.get(index++);
         }
