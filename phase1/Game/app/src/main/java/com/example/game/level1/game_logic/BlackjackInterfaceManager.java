a9468ec829f63c24608325e59d20ffcbdb791b0b
Lukas <lukas.willsie@mail.utoronto.ca>
2019-10-31 16:47:01 -0700

    Added documentation to Suit, BlackjackInterfaceManager, and InterfaceManager

--- phase1/Game/app/src/main/java/com/example/game/level1/game_logic/BlackjackInterfaceManager.java
+++ phase1/Game/app/src/main/java/com/example/game/level1/game_logic/BlackjackInterfaceManager.java
@@ -8,14 +8,31 @@ import com.example.game.level1.domain.Player;
 
 
 public class BlackjackInterfaceManager implements InterfaceManager {
+    /**
+     * The object acting as interpreter for the user of the app
+     */
     private PlayerInterpreter userInterpreter;
+
+    /**
+     * The object acting as interpreter for the dealer in the game
+     */
     private PlayerInterpreter dealerInterpreter;
 
+    /**
+     * Creates a new BlackjackInterfaceManager with the given information
+     * @param user - a Player object representing the user of the game
+     * @param dealer - a Player object representing the dealer in the game
+     * @param userHand - the TextView being used to display the user's hand on the screen
+     * @param dealerHand - the TextView being used to display the dealer's hand on the screen
+     */
     public BlackjackInterfaceManager(Player user, Player dealer, TextView userHand, TextView dealerHand) {
         userInterpreter = new PlayerInterpreter(user, new PlayerHandView(userHand));
         dealerInterpreter = new PlayerInterpreter(dealer, new PlayerHandView(dealerHand));
     }
 
+    /**
+     * Update the interface by delegating to userInterpreter and dealerInterpreter
+     */
     public void update() {
         userInterpreter.updatePlayerHand();
 
