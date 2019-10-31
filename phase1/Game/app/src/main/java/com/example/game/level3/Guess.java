8a83bf6ad1d930c5d8c3bb2429b4d60b3ddbbac1
Peter <peter.potaptchik@mail.utoronto.ca>
2019-10-30 20:30:14 -0400

    Added a method that given an array, returns the element : count pairs. This is a helper method for another method.

--- phase1/Game/app/src/main/java/com/example/game/level3/Guess.java
+++ phase1/Game/app/src/main/java/com/example/game/level3/Guess.java
@@ -1,5 +1,7 @@
 package com.example.game.level3;
 
+import java.util.HashMap;
+
 /**
  * A class that deals with a guess that a player makes.
  */
@@ -35,7 +37,7 @@ public class Guess {
      * A method that returns the number of cows (the number of elements that were correctly guessed
      * and in the right position).
      */
-    public int getCows() {
+    private int getCows() {
 
         int numberCows = 0;
 
@@ -46,6 +48,52 @@ public class Guess {
         }
 
         return numberCows;
+    }
+
+    /**
+     * A method that returns a HashMap representation of an array where the keys are the elements in
+     * the array and they values are the number of occurrences of this element in the the array.
+     *
+     * @param array The array to be processed.
+     * @return The HashMap of element ot count pairs.
+     */
+    private HashMap<String, Integer> createMap(String[] array) {
+        int arraySize = array.length;
+        HashMap<String, Integer> arrayElements = new HashMap<>();
+
+
+        for (int i = 0; i < arraySize; i++) {
+            String element = array[i];
+            if (!arrayElements.containsKey(element)) {
+                arrayElements.put(element, 1);
+            } else {
+                arrayElements.put(element, arrayElements.get(element) + 1);
+            }
+        }
+
+        return arrayElements;
+
+    }
+
+    /**
+     * A method that returns the number of elements that were correctly guessed irrespective of
+     * the order. Note that if the same element is guessed multiple times, then at most the number
+     * of times this element appears in the answer is returned.
+     */
+    private int getCorrectElements() {
+        HashMap<String, Integer> guessElements = new HashMap<>();
+        HashMap<String, Integer> answerElements = new HashMap<>();
+
+        for (int i = 0; i < this.guessSize; i++) {
+            String guessElement = this.guessArray[i];
+            String answerElement = this.answerArray[i];
+            if (!guessElements.containsKey(guessElement)) {
+                guessElements.put(guessElement, 1);
+            } else {
+                guessElements.put(guessElement, guessElements.get(guessElement) + 1);
+            }
+        }
+
     }
 
 }
