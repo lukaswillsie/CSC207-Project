1ff3901b9267eb3b4fa7663e1c3d0d249df70f44
Vaskar <vaskar.nath@mail.utoronto.ca>
2019-10-31 01:13:05 -0400

    Added a Chronometer to CowsBullsActivity

--- phase1/Game/app/src/main/java/com/example/game/CowsBullsActivity.java
+++ phase1/Game/app/src/main/java/com/example/game/CowsBullsActivity.java
@@ -3,12 +3,20 @@ package com.example.game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Chronometer;
+import android.widget.TextView;
 
 public class CowsBullsActivity extends AppCompatActivity {
+    private TextView timer;
+    private Chronometer chronometer;
+    private long elapsedTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cows_bulls);
+
+        chronometer = (Chronometer) findViewById(R.id.timer);
+        chronometer.start();
     }
 }
