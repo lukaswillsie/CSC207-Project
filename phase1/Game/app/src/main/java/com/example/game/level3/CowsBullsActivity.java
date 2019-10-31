533f0d220d1f88e7284945d77321ec22e4eeccfc
Lukas <lukas.willsie@mail.utoronto.ca>
2019-10-31 16:59:17 -0700

    Reformatted level 3 code

--- phase1/Game/app/src/main/java/com/example/game/level3/CowsBullsActivity.java
+++ phase1/Game/app/src/main/java/com/example/game/level3/CowsBullsActivity.java
@@ -1,10 +1,10 @@
 package com.example.game.level3;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.Chronometer;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.game.R;
 
@@ -18,7 +18,7 @@ public class CowsBullsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cows_bulls);
 
-        chronometer = (Chronometer) findViewById(R.id.timer);
+        chronometer = findViewById(R.id.timer);
         chronometer.start();
     }
 }
