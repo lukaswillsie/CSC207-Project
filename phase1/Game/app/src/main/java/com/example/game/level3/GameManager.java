7aecf645c8fd48cb27b8710ecd6120b0199cb90e
Peter <peter.potaptchik@mail.utoronto.ca>
2019-11-01 14:44:56 -0400

    Debugging changes.

--- phase1/Game/app/src/main/java/com/example/game/level3/GameManager.java
+++ phase1/Game/app/src/main/java/com/example/game/level3/GameManager.java
@@ -38,6 +38,7 @@ class GameManager {
             int rand = (int) (Math.random() * alphabet.length);
 
             this.answerArray[i] = alphabet[rand];
+            System.out.println(this.answerArray[i]);
         }
 
     }
