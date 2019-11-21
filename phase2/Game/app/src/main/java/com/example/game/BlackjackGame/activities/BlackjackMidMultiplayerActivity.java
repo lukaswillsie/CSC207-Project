14bfe9809b0ef00dfa82c76ead09212c46f7375b
Lukas <lukas.willsie@mail.utoronto.ca>
2019-11-20 17:35:48 -0800

    Added Activity for displaying Player 1's statistics to the users before Player 2's turn happens

---
+++ phase2/Game/app/src/main/java/com/example/game/BlackjackGame/activities/BlackjackMidMultiplayerActivity.java
@@ -0,0 +1,15 @@
+package com.example.game.BlackjackGame.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.game.R;
+
+public class BlackjackMidMultiplayerActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mid_multiplayer);
+    }
+}
