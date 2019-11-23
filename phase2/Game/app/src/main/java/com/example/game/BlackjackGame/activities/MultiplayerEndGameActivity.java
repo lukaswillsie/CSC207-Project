8b3f113863924941ca17a766b04a2603fd9893e0
Lukas <lukas.willsie@mail.utoronto.ca>
2019-11-23 00:01:42 -0800

    Updated MultiplayerEndGameActivity to properly display the players' statistics

--- phase2/Game/app/src/main/java/com/example/game/BlackjackGame/activities/MultiplayerEndGameActivity.java
+++ phase2/Game/app/src/main/java/com/example/game/BlackjackGame/activities/MultiplayerEndGameActivity.java
@@ -5,19 +5,56 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.game.MainActivity;
 import com.example.game.R;
+import com.example.game.services.MultiplayerDataManager;
+import com.example.game.services.TestMultiplayerDataManager;
+
+import java.text.DecimalFormat;
+
+import static com.example.game.data.MultiplayerDoubleData.BLACKJACK_PLAYER_1_WIN_RATE;
+import static com.example.game.data.MultiplayerDoubleData.BLACKJACK_PLAYER_2_WIN_RATE;
+import static com.example.game.data.MultiplayerIntData.BLACKJACK_PLAYER1_LONGEST_STREAK;
+import static com.example.game.data.MultiplayerIntData.BLACKJACK_PLAYER2_LONGEST_STREAK;
 
 public class MultiplayerEndGameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multiplayer_end_game);
+
+        initializeTextViews();
     }
 
     public void mainMenu(View view){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    /**
+     * Initialize the text views on the screen; populate the stats field with the players' stats,
+     * and update the tags to show the players' username
+     */
+    public void initializeTextViews(){
+        // TODO: Write an actual implementation and remove this dummy class
+        MultiplayerDataManager manager = new TestMultiplayerDataManager();
+        String player1StatsLabel = manager.getPlayer1Username();
+        String player1WinRate = new DecimalFormat("##.##").format(manager.getMultiplayerData(BLACKJACK_PLAYER_1_WIN_RATE)) + "%";
+        String player1LongestStreak = "" + manager.getMultiplayerData(BLACKJACK_PLAYER1_LONGEST_STREAK);
+
+        String player2StatsLabel = manager.getPlayer2Username();
+        String player2WinRate = new DecimalFormat("##.##").format(manager.getMultiplayerData(BLACKJACK_PLAYER_2_WIN_RATE)) + "%";
+        String player2LongestStreak = "" + manager.getMultiplayerData(BLACKJACK_PLAYER2_LONGEST_STREAK);
+
+
+        ((TextView)findViewById(R.id.player1StatsLabel)).setText(player1StatsLabel);
+        ((TextView)findViewById(R.id.player1WinRateDisplay)).setText(player1WinRate);
+        ((TextView)findViewById(R.id.player1LongestStreakDisplay)).setText(player1LongestStreak);
+
+        ((TextView)findViewById(R.id.player2StatsLabel)).setText(player2StatsLabel);
+        ((TextView)findViewById(R.id.player2WinRateDisplay)).setText(player2WinRate);
+        ((TextView)findViewById(R.id.player2LongestStreakDisplay)).setText(player2LongestStreak);
     }
 }
