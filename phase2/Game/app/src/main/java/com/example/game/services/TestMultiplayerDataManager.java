88b8754fb6d5a71b08732ca1317b891c19bb3661
Lukas <lukas.willsie@mail.utoronto.ca>
2019-11-23 13:20:16 -0800

    Updated the dummy implementation of MultiplayerDataManager to use static variables so that information can actually be stored and updated for testing purposes. Before it was returning fake, hard-coded constants.

--- phase2/Game/app/src/main/java/com/example/game/services/TestMultiplayerDataManager.java
+++ phase2/Game/app/src/main/java/com/example/game/services/TestMultiplayerDataManager.java
@@ -5,48 +5,84 @@ import com.example.game.data.MultiplayerIntData;
 
 public class TestMultiplayerDataManager implements MultiplayerDataManager {
     private static int turn = 1;
-    @Override
-    public void setPlayer1Username(String player1Username) {
+    private static String player1Username;
+    private static int player1LongestStreak = 0;
+    private static double player1WinRate = 0.0;
 
+    private static String player2Username;
+    private static int player2LongestStreak = 0;
+    private static double player2WinRate = 0.0;
+
+    @Override
+    public void setPlayer1Username(String newPlayer1Username) {
+        player1Username = newPlayer1Username;
     }
 
     @Override
-    public void setPlayer2Username(String player2Username) {
-
+    public void setPlayer2Username(String newPlayer2Username) {
+        player2Username = newPlayer2Username;
     }
 
     @Override
     public String getPlayer1Username() {
-        return "lukas";
+        return player1Username;
     }
 
     @Override
     public String getPlayer2Username() {
-        return "peter";
+        return player2Username;
     }
 
     @Override
     public void setMultiplayerData(MultiplayerIntData dataType, int newValue) {
-        if(dataType == MultiplayerIntData.BLACKJACK_PLAYER_TURN){
-            turn = newValue;
+        switch(dataType){
+            case BLACKJACK_PLAYER_TURN:
+                turn = newValue;
+                break;
+            case BLACKJACK_PLAYER1_LONGEST_STREAK:
+                player1LongestStreak = newValue;
+                break;
+            case BLACKJACK_PLAYER2_LONGEST_STREAK:
+                player2LongestStreak = newValue;
+                break;
         }
     }
 
     @Override
     public void setMultiplayerData(MultiplayerDoubleData dataType, double newValue) {
-
+        switch (dataType){
+            case BLACKJACK_PLAYER_1_WIN_RATE:
+                player1WinRate = newValue;
+                break;
+            case BLACKJACK_PLAYER_2_WIN_RATE:
+                player2WinRate = newValue;
+                break;
+        }
     }
 
     @Override
     public int getMultiplayerData(MultiplayerIntData dataType) {
-        if(dataType == MultiplayerIntData.BLACKJACK_PLAYER_TURN){
-            return turn;
+        switch(dataType){
+            case BLACKJACK_PLAYER_TURN:
+                return turn;
+            case BLACKJACK_PLAYER1_LONGEST_STREAK:
+                return player1LongestStreak;
+            case BLACKJACK_PLAYER2_LONGEST_STREAK:
+                return player2LongestStreak;
+            default:
+                return 0;
         }
-        return 12;
     }
 
     @Override
     public double getMultiplayerData(MultiplayerDoubleData dataType) {
-        return 75.00;
+        switch (dataType){
+            case BLACKJACK_PLAYER_1_WIN_RATE:
+                return player1WinRate;
+            case BLACKJACK_PLAYER_2_WIN_RATE:
+                return player2WinRate;
+            default:
+                return 0.0;
+        }
     }
 }
