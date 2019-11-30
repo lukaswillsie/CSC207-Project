2003e2ed86e1c892ed5d5b467df24853c05b39d8
Peter <peter.potaptchik@mail.utoronto.ca>
2019-11-30 16:54:37 -0500

    Add a class that is responsible for deciding whether a given score should be recorded as a best score for any of the games.

--- phase2/Game/app/src/main/java/com/example/game/CowsBullsGame/activities/CowsBullsActivityPresenter.java
+++
@@ -1,4 +0,0 @@
-package com.example.game.CowsBullsGame.activities;
-
-public class CowsBullsActivityPresenter {
-}
--- phase2/Game/app/src/main/java/com/example/game/services/scoreboard/BestScoreSelector.java
+++ phase2/Game/app/src/main/java/com/example/game/services/scoreboard/BestScoreSelector.java
@@ -1,4 +1,101 @@
 package com.example.game.services.scoreboard;
 
-public class BestScoreSelector {
+import android.util.Pair;
+
+import java.util.List;
+
+/**
+ * A class that selects the helps select the best scores for a game.
+ */
+class BestScoreSelector {
+
+    // The Game type for this game.
+    private ScoreboardRepository.Game game;
+
+    // The number of top scores to select.
+    private int numScores = 10;
+
+    /**
+     * A constructor for the BestScoreSelector class.
+     *
+     * @param game the game to select the best score from
+     */
+    public BestScoreSelector(ScoreboardRepository.Game game, boolean high) {
+        this.game = game;
+    }
+
+    /**
+     * Returns whether or not the user should be prompted to save the given score.
+     *
+     * @param score - the score to check
+     * @return whether or not the game should prompt the user to save the given score
+     */
+    public boolean shouldPrompt(int score) {
+
+        switch (game) {
+            case BLACKJACK:
+                return shouldPromptHigh(score);
+            case COWS_AND_BULLS:
+                return shouldPromptLow(score);
+            default:
+                return false;
+        }
+    }
+
+    // Helper methods to shouldPrompt.
+
+    /**
+     * Returns whether or not the user should be prompted to save the given score. This method
+     * should be called when the highest scores are the best scores.
+     *
+     * @param score - the score to check
+     * @return whether or not the game should prompt the user to save the given score
+     */
+    private boolean shouldPromptHigh(int score) {
+        ScoreboardRepository highestScoreManager = new ScoreboardRepositoryFactory().build(game);
+
+        List<Pair<String, Integer>> highestScores = highestScoreManager.getHighScores(numScores);
+
+        // This score is one of the first few scores, so it should be a best score.
+        if (highestScores.size() < numScores) {
+            return true;
+        }
+
+        // If score is greater than any of the previous 10 highest scores, it should be a new best score.
+        for (Pair<String, Integer> pair : highestScores) {
+            if (pair.second < score) {
+                return true;
+            }
+        }
+        // If the top 10 scores are all >= score, then return false.
+        return false;
+    }
+
+    /**
+     * Returns whether or not the user should be prompted to save the given score. This method
+     * should be called when the lowest scores are the best scores.
+     *
+     * @param score - the score to check
+     * @return whether or not the game should prompt the user to save the given score
+     */
+    private boolean shouldPromptLow(int score) {
+        ScoreboardRepository lowestScoreManager = new ScoreboardRepositoryFactory().build(game);
+
+        List<Pair<String, Integer>> lowestScores = lowestScoreManager.getLowScores(numScores);
+
+        // This score is one of the first few scores, so it should be a best score.
+        if (lowestScores.size() < numScores) {
+            return true;
+        }
+
+        // If score is less than any of the previous 10 lowest scores, it should be a new best score.
+        for (Pair<String, Integer> pair : lowestScores) {
+            if (pair.second > score) {
+                return true;
+            }
+        }
+        // If the top 10 scores are all <= score, then return false.
+        return false;
+    }
 }
+
