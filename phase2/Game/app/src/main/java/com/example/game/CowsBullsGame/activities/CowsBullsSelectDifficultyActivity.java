c1f518e37bfeb21e25a159dfd688be3ffc144b2a
Vaskar <vaskar.nath@mail.utoronto.ca>
2019-11-30 15:06:35 -0500

    Added a CowsBullsSelectDifficultyActvivity and updated wording for SettingsActivity

---
+++ phase2/Game/app/src/main/java/com/example/game/CowsBullsGame/activities/CowsBullsSelectDifficultyActivity.java
@@ -0,0 +1,16 @@
+package com.example.game.CowsBullsGame.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.game.R;
+
+public class CowsBullsSelectDifficultyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cows_bulls_select_difficulty);
+    }
+}
