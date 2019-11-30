87ec2764037f9315c96367c997958c9708bada5d
Valeria Fakhrutdinova <valeriia.fakhrutdinova@mail.utoronto.ca>
2019-11-30 18:31:18 -0500

    Local server scoreboadr is working for all games

--- phase2/Game/app/src/main/java/com/example/game/StatsActivity.java
+++ phase2/Game/app/src/main/java/com/example/game/StatsActivity.java
@@ -1,6 +1,8 @@
 package com.example.game;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -38,5 +40,30 @@ public class StatsActivity extends AppCompatActivity {
 
         TextView longestStreak = findViewById(R.id.longestStreak);
         longestStreak.setText(String.valueOf(statsManager.getStat(Statistic.LONGEST_STREAK)));
+    }
+
+    public void GuessNumStats(View view){
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("key", Statistic.FEWEST_GUESSES);
+
+        Intent intent = new Intent(this, StatsLocalScoreBoardActivity.class);
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
+
+    public void CowsAndBullsStats(View view){
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("key", Statistic.QUICKEST_TIME);
+        Intent intent = new Intent(this, StatsLocalScoreBoardActivity.class);
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
+
+    public void BlackJackStats(View view){
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("key", Statistic.LONGEST_STREAK);
+        Intent intent = new Intent(this, StatsLocalScoreBoardActivity.class);
+        intent.putExtras(bundle);
+        startActivity(intent);
     }
 }
