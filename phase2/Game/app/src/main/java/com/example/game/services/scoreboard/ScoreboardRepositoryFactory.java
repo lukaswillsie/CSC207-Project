8cf40a7f260bbd1ff88a28c622ea2cf911765eb9
Peter <peter.potaptchik@mail.utoronto.ca>
2019-11-30 21:08:22 -0500

    Minor refactoring of ScoreboardRepositoryFactory.

--- phase2/Game/app/src/main/java/com/example/game/services/scoreboard/ScoreboardRepositoryFactory.java
+++ phase2/Game/app/src/main/java/com/example/game/services/scoreboard/ScoreboardRepositoryFactory.java
@@ -8,27 +8,29 @@ import java.io.File;
 public class ScoreboardRepositoryFactory {
     /**
      * Creates and returns a ScoreboardRepository object to manage scoreboard data for the given
-     * game
+     * game.
+     *
      * @param game - the game whose scoreboard data client code wants to edit/access
      * @return a ScoreboardRepository object prepared to manage scoreboard data for the given game
      */
-    public ScoreboardRepository build(ScoreboardRepository.Game game){
+    public ScoreboardRepository build(ScoreboardRepository.Game game) {
         File filesDir = new File(GameData.filesDirPath);
-        File highscoreFile = new File(filesDir, filename(game));
+        File scoreFile = new File(filesDir, filename(game));
 
         ScoreboardFileManager repository = new ScoreboardFileManager();
-        repository.setFile(highscoreFile);
+        repository.setFile(scoreFile);
 
         return repository;
     }
 
     /**
      * Return the name of the file that is being used to store scoreboard data for the given game
+     *
      * @param game - the game whose scoreboard data file should be returned
      * @return the name of the file used to store scoreboard data for the given game
      */
-    private String filename(ScoreboardRepository.Game game){
-        switch(game){
+    private String filename(ScoreboardRepository.Game game) {
+        switch (game) {
             case BLACKJACK:
                 return GameConstants.BLACKJACK_HIGHSCORE_FILE;
             case COWS_AND_BULLS:
