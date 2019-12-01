099b3e46d5256ac30f443e669203ae3ef0ed3ccd
Lukas <lukas.willsie@mail.utoronto.ca>
2019-12-01 13:19:40 -0800

    Corrected some documentation in UserAccountManager

--- phase2/Game/app/src/main/java/com/example/game/services/accounts/UserAccountManager.java
+++ phase2/Game/app/src/main/java/com/example/game/services/accounts/UserAccountManager.java
@@ -22,15 +22,15 @@ import static com.example.game.data.GameConstants.USERS_DIR_NAME;
 /**
  * An implementation of AccountManager that uses a folder structures like the following:
  * app_root_directory\
- * users\
- * lukas
- * settings
- * stats
- * password
- * peter
- * settings
- * stats
- * password
+ *  users\
+ *      lukas
+ *          settings
+ *          stats
+ *          password
+ *      peter
+ *          settings
+ *          stats
+ *          password
  * to query and create users and their information.
  * Please note that we know its bad to store passwords in plain text, we will work on fixing this
  * for phase 2.
