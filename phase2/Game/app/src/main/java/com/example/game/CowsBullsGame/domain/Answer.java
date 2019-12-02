bf5f2492d9ae4cb896beb3271aca43ee5a9eea80
Peter <peter.potaptchik@mail.utoronto.ca>
2019-12-01 23:49:34 -0500

    Removing print statements.

--- phase2/Game/app/src/main/java/com/example/game/CowsBullsGame/domain/Answer.java
+++ phase2/Game/app/src/main/java/com/example/game/CowsBullsGame/domain/Answer.java
@@ -53,7 +53,6 @@ public class Answer {
             // Generate a random integer from 0 to alphabet.length - 1 with equal probabilities.
             int rand = (int) (Math.random() * alphabet.length);
             this.answerArray[i] = alphabet[rand];
-            System.out.println(answerArray[i]);
         }
 
         alphabetList = Arrays.asList(alphabet);
