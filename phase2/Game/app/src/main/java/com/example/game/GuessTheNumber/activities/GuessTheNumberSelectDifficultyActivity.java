11fb6ed803420d01d1efe91070d21c29c5f105c5
Elexandra Tran <elexandra.tran@utoronto.ca>
2019-12-01 22:07:45 -0500

    Implemented so that difficulty settings are updated when the user clicks on the respective difficulty in GuessTheNumberSelectDifficultyActivity.

--- phase2/Game/app/src/main/java/com/example/game/GuessTheNumber/activities/GuessTheNumberSelectDifficultyActivity.java
+++ phase2/Game/app/src/main/java/com/example/game/GuessTheNumber/activities/GuessTheNumberSelectDifficultyActivity.java
@@ -5,27 +5,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.game.R;
+import com.example.game.data.GameData;
+import com.example.game.data.Setting;
+import com.example.game.services.settings.SettingsManager;
+import com.example.game.services.settings.SettingsManagerFactory;
 
 public class GuessTheNumberSelectDifficultyActivity extends AppCompatActivity {
+    SettingsManager settingsManager;
+    TextView currDiff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guess_the_number_select_difficulty);
+        String username = GameData.USERNAME;
+        settingsManager = new SettingsManagerFactory().build(this, username);
+        currDiff = findViewById(R.id.currDiffLevel);
     }
 
     public void easyDifficultyClick(View view) {
-
+        settingsManager.updateSetting(Setting.GUESS_THE_NUMBER_RANGE, 50);
+        currDiff.setText("Easy");
     }
 
     public void mediumDifficultyClick(View view) {
-
+        settingsManager.updateSetting(Setting.GUESS_THE_NUMBER_RANGE, 100);
+        currDiff.setText("Medium");
     }
 
     public void insaneDifficultyClick(View view) {
-
+        settingsManager.updateSetting(Setting.GUESS_THE_NUMBER_RANGE, 1000);
+        currDiff.setText("Medium");
     }
 
     public void backClick(View view) {
